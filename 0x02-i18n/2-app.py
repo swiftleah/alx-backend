3d316947476db098f707128a965e164c3779ca5f
swiftleah <leahrosehenry@gmail.com>
2024-04-09 12:41:26 +0000

    Task 2

---
+++ 0x02-i18n/2-app.py
@@ -0,0 +1,22 @@
+#!/usr/bin/env python3
+''' flask application code '''
+from flask import Flask, render_template, request
+from flask_babel import Babel, _
+
+
+app = Flask(__name__)
+babel = Babel(app)
+
+
+@babel.localeselector
+def get_locale():
+    return request.accept_languages.best_match(app.config['LANGUAGES'])
+
+
+@app.route('/')
+def index():
+    return render_template('2-index.html')
+
+
+if __name__ == '__main__':
+    app.run()
