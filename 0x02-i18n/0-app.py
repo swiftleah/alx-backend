372915bcb733f1ec4a3b1e0278305a5aaf6e0cef
swiftleah <leahrosehenry@gmail.com>
2024-04-09 11:56:43 +0000

    Task 0

--- 0x02-i18n/0-app.py
+++ 0x02-i18n/0-app.py
@@ -7,7 +7,7 @@ app = Flask(__name__)
 
 
 @app.route('/', strict_slashes=False)
-def index() - > str:
+def index() -> str:
     ''' renders basic html template '''
     return render_template('0-index.html')
 
