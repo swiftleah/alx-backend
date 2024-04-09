1652d2e68dce906e355fb19bcec3108ecfe728d2
swiftleah <leahrosehenry@gmail.com>
2024-04-09 11:54:37 +0000

    Task 0

--- 0x02-i18n/0-app.py
+++ 0x02-i18n/0-app.py
@@ -4,13 +4,13 @@ from Flask import Flask, render_template
 
 
 app = Flask(__name__)
-''' creates Flask application instance '''
 
 
-@app.route('/')
+@app.route('/', strict_slashes=False)
 def index() - > str:
     ''' renders basic html template '''
     return render_template('0-index.html')
+
 
 if __name__ == "__main__":
     app.run()
